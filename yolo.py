f35ebd42d8c7177a9674025ef227f37681ccbf1a
mm0177 <113229869+mm0177@users.noreply.github.com>
2024-03-23 00:19:28 +0530

    Add files via upload

---
+++ yolo.py
@@ -0,0 +1,9 @@
+from ultralytics import YOLO
+
+# Load a model
+model = YOLO("yolov8n.yaml")  # build a new model from scratch
+
+
+# Use the model
+results=model.train(data="data.yaml", epochs=15)  # train the model
+ 
